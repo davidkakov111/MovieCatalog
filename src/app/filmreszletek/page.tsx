8e893fc9949d77c5ec7846c59b84da7d9e252cb3
David Kovacs <143946579+davidkakov111@users.noreply.github.com>
2024-01-27 15:52:24 +0200

    Mukodik lokalaisan (javitva 2)

--- src/app/filmreszletek/page.tsx
+++ src/app/filmreszletek/page.tsx
@@ -1,5 +1,5 @@
 "use client"
-import React, { useEffect, useState } from 'react';
+import React, { useEffect, useState, Suspense } from 'react';
 import { useSearchParams } from 'next/navigation';
 
 const fetchFilmDetails = async (cim:any) => {
@@ -39,7 +39,7 @@ const FilmReszletek: React.FC = () => {
       }
     };
 
-    fetchData(); // Nem szükséges az "if (typeof window !== 'undefined')" ellenőrzés, mert az useEffect csak a kliensoldali rendereléskor fut le
+    fetchData(); 
   }, [params]);
 
   if (!params || isLoading) {
@@ -54,13 +54,15 @@ const FilmReszletek: React.FC = () => {
   const formattedDate = new Date(film_adatok.megjelenes_datuma).toLocaleDateString();
 
   return (
-    <div className="max-w-2xl mx-auto mt-8 p-4 bg-gray-100 rounded-md">
-      <h2 className="text-3xl font-bold mb-4 text-center text-gray-800">{film_adatok.cim}</h2>
-      <img className="w-full h-auto" src={film_adatok.poszter_url} alt={film_adatok.cim} />
-      <p className="text-gray-700 mb-4">Megjelenés dátuma: {formattedDate}</p>
-      <p className="text-gray-700 mb-4">Értékelés: {film_adatok.ertekeles}</p>
-      <p className="text-gray-700 mb-4">{film_adatok.leiras}</p>
-    </div>
+    <Suspense fallback={<h1>Betöltés...</h1>}>
+      <div className="max-w-2xl mx-auto mt-8 p-4 bg-gray-100 rounded-md">
+        <h2 className="text-3xl font-bold mb-4 text-center text-gray-800">{film_adatok.cim}</h2>
+        <img className="w-full h-auto" src={film_adatok.poszter_url} alt={film_adatok.cim} />
+        <p className="text-gray-700 mb-4">Megjelenés dátuma: {formattedDate}</p>
+        <p className="text-gray-700 mb-4">Értékelés: {film_adatok.ertekeles}</p>
+        <p className="text-gray-700 mb-4">{film_adatok.leiras}</p>
+      </div>
+    </Suspense>
   );
 };
 
