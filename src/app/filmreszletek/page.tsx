7a54bec3eeeeb086ceb0f97cf918c4b5ea08c1bc
David Kovacs <143946579+davidkakov111@users.noreply.github.com>
2024-01-27 14:55:33 +0200

    Hook ...

--- src/app/filmreszletek/page.tsx
+++ src/app/filmreszletek/page.tsx
@@ -1,70 +1,37 @@
 "use client"
-import React, { useEffect, useState, Suspense } from 'react';
+import React from 'react';
 import { useSearchParams } from 'next/navigation';
 
-const fetchFilmDetails = async (cim: any) => {
-  try {
-    const response = await fetch(`/api/filmDetailsBycim?title=${encodeURIComponent(cim)}`);
-    const data = await response.json();
-    return data;
-  } catch (error) {
-    console.error('Hiba történt a film részleteinek lekérése közben:', error);
-    return null;
-  }
-};
-
-const FilmReszletekContent: React.FC<{ filmDetails: any }> = ({ filmDetails }) => {
-  const film_adatok = filmDetails[0][0];
-  const formattedDate = new Date(film_adatok.megjelenes_datuma).toLocaleDateString();
-
-  return (
-    <div className="max-w-2xl mx-auto mt-8 p-4 bg-gray-100 rounded-md">
-      <h2 className="text-3xl font-bold mb-4 text-center text-gray-700">{film_adatok.cim}</h2>
-      <img className="w-full h-auto" src={film_adatok.poszter_url} alt={film_adatok.cim} />
-      <p className="text-gray-700 mb-4">Megjelenés dátuma: {formattedDate}</p>
-      <p className="text-gray-700 mb-4">Értékelés: {film_adatok.ertekeles}</p>
-      <p className="text-gray-700 mb-4">{film_adatok.leiras}</p>
-    </div>
-  );
-};
-
-const FilmReszletek: React.FC = () => {
+export default async function FilmReszletek () {
+  // URL paraméterek lekérése
   const params = useSearchParams();
-  const [filmDetails, setFilmDetails] = useState<any | null>(null);
-  const [isLoading, setIsLoading] = useState(true);
-
-  useEffect(() => {
-    const fetchData = async () => {
-      if (!params) {
-        return;
-      }
-
-      const cim = params.get('title');
-      if (cim === null) {
-        return;
-      }
-
-      const data = await fetchFilmDetails(cim);
-      setFilmDetails(data);
-      setIsLoading(false);
-    };
-
-    fetchData();
-  }, [params]);
-
-  if (!params || isLoading) {
-    return <h1>{isLoading ? 'Betöltés...' : 'Ismeretlen film!'}</h1>;
+  // Ha nincs paraméter, megjelenítem a megfelelő üzenetet
+  if (!params) {
+    return <h1>Nem érkezett paraméter!</h1>;
   }
-
-  if (filmDetails !== null) {
-    return (
-      <Suspense fallback={<div>Betöltés...</div>}>
-        <FilmReszletekContent filmDetails={filmDetails} />
-      </Suspense>
-    );
-  } else {
+  // A film címének kinyerése a paraméterekből
+  const cim = params?.get('title');
+  // Ellenőrizem, hogy van-e cím, mielőtt lekérdezném a részleteket
+  if (cim === null){
+    return <h1>Nem érkezett meg a cim! Nem tudom mely film adatait kell megjelenitsem!</h1>;
+  }
+  // Fetch kérés a szerverhez a film részletekért
+  const response = await fetch(`/api/filmDetailsBycim?title=${encodeURIComponent(cim)}`);
+  // JSON válasz feldolgozása
+  const data = await response.json();
+  if (data === null) {
     return <h1>Nincsenek adatok a filmről!</h1>;
   }
-};
-
-export default FilmReszletek;
+  // Ha vannak film részletek, formázom a dátumot, és megjelenítem azokat
+  const film_adatok = data[0][0];
+    const formattedDate = new Date(film_adatok.megjelenes_datuma).toLocaleDateString();
+    return (
+      <div className="max-w-2xl mx-auto mt-8 p-4 bg-gray-100 rounded-md">
+        <h2 className="text-3xl font-bold mb-4 text-center text-black">{film_adatok.cim}</h2>
+        <img className="w-full h-auto" src={film_adatok.poszter_url} alt={film_adatok.cim} />
+        <p className="text-gray-700 mb-4">Megjelenés dátuma: {formattedDate}</p>
+        <p className="text-gray-700 mb-4">Értékelés: {film_adatok.ertekeles}</p>
+        <p className="text-gray-700 mb-4">{film_adatok.leiras}</p>
+      </div>
+    );
+}
