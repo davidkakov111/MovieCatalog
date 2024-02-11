97da07c0fb63c8bf17de61e4eee860df7a2f387e
David Kovacs <143946579+davidkakov111@users.noreply.github.com>
2024-02-11 17:16:06 +0200

    Rate stars

--- src/app/components/search.tsx
+++ src/app/components/search.tsx
@@ -54,7 +54,7 @@ export default function Search({ placeholder, extracted }: SearchProps) {
       {/* Filtered movies can be displayed or utilized here */}
       <div>
         {filteredMovies.map((movie, index) => (
-          <FilmComponent key={index} title={movie.title} imageUrl={movie.poster_url}/>
+          <FilmComponent key={index} title={movie.title} imageUrl={movie.poster_url} rating={movie.rating}/>
         ))}
       </div>
     </div>
